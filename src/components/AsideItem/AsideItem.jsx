74a838d29c2dea5f4a49089f469c31a3508111b7
ReDAndFel <pipecar366@gmail.com>
2024-04-24 18:00:24 -0500

    Se implementó que al seleccionar una opcion del aside menu esta quedara en color

--- src/components/AsideItem/AsideItem.jsx
+++ src/components/AsideItem/AsideItem.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 import "./AsideItem.css"
 
-const AsideItem = ({ href, children }) => {
+const AsideItem = ({ href, children, selected, onClick }) => {
     return (
-        <li className="aside-item">
+        <li className={`aside-item${selected ? " selected":""}`} onClick={onClick}>
             <Link to={href}>{children}</Link>
         </li>
     )
--- src/components/AsideMenu/AsideMenu.jsx
+++ src/components/AsideMenu/AsideMenu.jsx
@@ -1,15 +1,23 @@
 import "./AsideMenu.css"
 import AsideItem from "../AsideItem/AsideItem"
+import { useState } from "react"
 
 const AsideMenu = () => {
+
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const handleItemClick = (index) => {
+        setSelectedItem(index);
+    };
+
     return (
         <nav className="aside-menu">
             <ul>
-                <AsideItem href="/">Inicio</AsideItem>
-                <AsideItem href="/">Gestionar examenes</AsideItem>
-                <AsideItem href="/">Gestionar preguntas</AsideItem>
-                <AsideItem href="/">Gestionar bancos de pregunta</AsideItem>
-                <AsideItem href="/">Responder examenes</AsideItem>
+                <AsideItem href="/" selected={selectedItem === 0} onClick={() => handleItemClick(0)}>Inicio</AsideItem>
+                <AsideItem href="/" selected={selectedItem === 1} onClick={() => handleItemClick(1)}>Gestionar examenes</AsideItem>
+                <AsideItem href="/" selected={selectedItem === 2} onClick={() => handleItemClick(2)}>Gestionar preguntas</AsideItem>
+                <AsideItem href="/" selected={selectedItem === 3} onClick={() => handleItemClick(3)}>Gestionar bancos de pregunta</AsideItem>
+                <AsideItem href="/" selected={selectedItem === 4} onClick={() => handleItemClick(4)}>Responder examenes</AsideItem>
             </ul>
         </nav>
     )
