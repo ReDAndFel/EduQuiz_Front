468f59015a7a05d3965ba5aef5a4a082d5c07614
ReDAndFel <pipecar366@gmail.com>
2024-04-24 13:32:03 -0500

    Se hizo el metodo de obtener todos los estudiantes en el hook useStudents

--- src/hooks/useStudent.jsx
+++ src/hooks/useStudent.jsx
@@ -1,5 +1,26 @@
-export const useStudent =() =>{
-    
-    
-    return {}
+import { useState } from "react";
+
+export const useStudent = () => {
+
+    const [listStudents, setListStudents] = useState([])
+
+    const API = ""
+
+    const getAllStudents = async () => {
+        fetch(`${API}/`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setListStudents(data.response)
+            })
+            .catch(error => {
+                console.error('Error en la solicitud http:', error);
+            });
+    }
+
+    return { getAllStudents, listStudents }
 }
