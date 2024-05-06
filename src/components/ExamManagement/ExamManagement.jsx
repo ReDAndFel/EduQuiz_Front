3131ccf3a76d7b09dee5c0dda183c1ff793f8d34
ReDAndFel <pipecar366@gmail.com>
2024-05-06 13:15:25 -0500

    Se implementó el hook setUniversity y el select de universidades

--- src/components/ExamManagement/ExamManagement.jsx
+++ src/components/ExamManagement/ExamManagement.jsx
@@ -1,20 +1,49 @@
 import SelectComponent from "../SelectComponent/SelectComponent";
 import "./ExamManagement.css"
 import { useCourse } from "../../hooks/useCourse";
+import { useUniversity } from "../../hooks/useUniversity";
 
 const ExamManagement = () => {
+    const { universities, handleChangeUniversity, selectedUniversity } = useUniversity()
     const { courses, handleChangeCourse, selectedCourse } = useCourse()
+    const { groups, handleChangeGroup, selectedGroup } = useGroup()
+    const { topics, handleChangeTopic, selectedTopic } = useTopic()
+
     return (
         <div className="exam-management-container">
             <h1>Gestionar examanes</h1>
 
-            <SelectComponent
-                list={courses}
-                onChange={handleChangeCourse}
-                defaultValue={selectedCourse}
-                firstOption="Seleccione un curso"
-            />
+            <form action="">
 
+                <SelectComponent
+                    list={universities}
+                    onChange={handleChangeUniversity}
+                    defaultValue={selectedUniversity}
+                    firstOption="Seleccione una universidad"
+                />
+
+                <SelectComponent
+                    list={courses}
+                    onChange={handleChangeCourse}
+                    defaultValue={selectedCourse}
+                    firstOption="Seleccione un curso"
+                />
+
+                <SelectComponent
+                    list={groups}
+                    onChange={handleChangeGroup}
+                    defaultValue={selectedGroup}
+                    firstOption="Seleccione un grupo"
+                />
+
+                <SelectComponent
+                    list={topics}
+                    onChange={handleChangeTopic}
+                    defaultValue={selectedTopic}
+                    firstOption="Seleccione un tema"
+                />
+
+            </form>
 
         </div>
     )
